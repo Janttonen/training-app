c955a309c5c80a388e12c664f7d44b26b3100573
Laura Janttonen <bgu252@myy.haaga-helia.fi>
2022-12-10 16:30:39 +0200

    Small changes2

--- src/components/CalendarPage.js
+++ src/components/CalendarPage.js
@@ -5,6 +5,15 @@ import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
 
+const CheckView = ({ info }) => {
+  const { training } = info;
+  return (
+    <div>
+      <p>{training.customer.firstname}</p>
+    </div>
+  );
+};
+
 function CalendarPage() {
   const [trainings, setTrainings] = useState([]);
 
@@ -27,33 +36,6 @@ function CalendarPage() {
       .catch((err) => console.error(err));
   };
 
-  const checkView = (prop) => {
-    if (prop.view.type == "dayGridMonth") {
-      return {
-        html:
-          prop.timeText +
-          "<br>" +
-          prop.event._def.extendedProps.customer.firstname +
-          " " +
-          prop.event._def.extendedProps.customer.lastname +
-          " - " +
-          prop.event._def.extendedProps.activity +
-          "<br>",
-      };
-    } else {
-      return {
-        html:
-          prop.timeText +
-          "<br>" +
-          prop.event.extendedProps.customer.firstname +
-          " " +
-          prop.event.extendedProps.customer.lastname +
-          " - " +
-          prop.event.extendedProps.activity +
-          "<br>",
-      };
-    }
-  };
 
   return (
     <>
@@ -68,7 +50,7 @@ function CalendarPage() {
           }}
           initialView="dayGridMonth"
           events={trainings}
-          eventContent={checkView}
+          eventContent={(info) => <CheckView info={trainings} />}
           eventTimeFormat={{
             hour: "2-digit",
             minute: "2-digit",
